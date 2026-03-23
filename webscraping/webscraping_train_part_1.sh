9ec80eb2e8b3c32732726933acd7903601d8a8f0
Karish-Gupta <karishgupta2003@gmail.com>
2026-03-23 14:32:58 -0400

    hotfix

--- webscraping/webscraping_train_part_1.sh
+++ webscraping/webscraping_train_part_1.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mem=64g
-#SBATCH -J "train_webscraping"
-#SBATCH -o train_webscraping%j.out
-#SBATCH -e train_webscraping%j.err
+#SBATCH -J "1_train_webscraping"
+#SBATCH -o 1_train_webscraping%j.out
+#SBATCH -e 1_train_webscraping%j.err
 #SBATCH -p short
 #SBATCH -t 24:00:00
 #SBATCH --gres=gpu:H200:1
