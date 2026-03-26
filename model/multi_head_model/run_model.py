8590bacf576dc616192ca262576fcdd821873905
Karish-Gupta <karishgupta2003@gmail.com>
2026-03-26 12:05:46 -0400

    Moved notebooks and update args for multi-head model

--- model/multi_head_model/run_model.py
+++ model/multi_head_model/run_model.py
@@ -7,9 +7,9 @@ from model.multi_head_model.dataset import NAICSDataset
 from model.multi_head_model.loss import HierarchicalLoss
 
 # Load data
-train_df = pd.read_csv("data/split_data/train.csv") 
-test_df = pd.read_csv("data/split_data/test.csv")
-val_df = pd.read_csv("data/split_data/val.csv")
+train_df = pd.read_csv("data/split_data_preprocessed/train_data_with_summaries.csv") 
+test_df = pd.read_csv("data/split_data_preprocessed/test_data_with_summaries.csv")
+val_df = pd.read_csv("data/split_data_preprocessed/val_data_with_summaries.csv")
 
 # Initialize Datasets
 train_dataset = NAICSDataset(train_df, model_name="answerdotai/ModernBERT-large")
